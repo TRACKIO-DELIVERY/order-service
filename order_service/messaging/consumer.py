58b141c1656270dfb816d3e1e34d691d299bb57a
Mirla Lopes <mirlaisabel11@gmail.com>
2025-07-15 15:19:32 -0300

    fix: Remove rabbitmq conf from .local envs

--- order_service/messaging/consumer.py
+++ order_service/messaging/consumer.py
@@ -1,5 +1,4 @@
 import asyncio
-import logging
 from typing import TYPE_CHECKING
 
 import aio_pika
@@ -26,15 +25,4 @@ async def consumer(queue_name: str) -> None:
 
         await queue.consume(on_message, no_ack=False)
 
-        logging.info(" [*] Waiting for messages. To exit press CTRL+C")
         await asyncio.Future()
-
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_until_complete(consumer("order.created.queue"))
-    except KeyboardInterrupt:
-        logging.info(" [*] Stopping consumer...")
-    finally:
-        loop.close()
