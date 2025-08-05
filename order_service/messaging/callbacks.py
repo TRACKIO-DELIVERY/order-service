769c5fb02810bc07b38690da828de5fe5f1aec81
Mirla Lopes <mirlaisabel11@gmail.com>
2025-08-05 14:00:56 -0300

    linter

--- order_service/messaging/callbacks.py
+++ order_service/messaging/callbacks.py
@@ -2,6 +2,7 @@ import logging
 
 import aio_pika.abc
 
+
 # Aqui fica o processamento
 async def on_message(message: aio_pika.abc.AbstractIncomingMessage) -> None:
     logging.info(f"[x] Processing message {message}")
