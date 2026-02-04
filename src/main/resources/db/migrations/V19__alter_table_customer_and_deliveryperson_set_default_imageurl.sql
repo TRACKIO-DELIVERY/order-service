661d6c07b25c3d2b1545705ecc15f4a688ab6ded
Mirla Lopes <mirlaisabel11@gmail.com>
2026-02-04 16:37:52 -0300

    feat: Set default image url for users

---
+++ src/main/resources/db/migrations/V19__alter_table_customer_and_deliveryperson_set_default_imageurl.sql
@@ -0,0 +1,5 @@
+ALTER TABLE customer ALTER COLUMN image_url SET DEFAULT 'https://trackio-images-storage.s3.us-east-2.amazonaws.com/Customer/211503022026user.jpg';
+
+ALTER TABLE company ALTER COLUMN image_url SET DEFAULT 'https://trackio-images-storage.s3.us-east-2.amazonaws.com/Company/211503022026user.jpg';
+
+ALTER TABLE delivery_person ALTER COLUMN image_url SET DEFAULT 'https://trackio-images-storage.s3.us-east-2.amazonaws.com/DeliveryPerson/211503022026user.jpg'
